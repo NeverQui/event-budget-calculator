734f2ec4499298b5d73db1e1e810e33e11fa52ef
Peter Hartman <peterhartman@mac-studio.home>
2025-04-19 23:12:55 -0400

    Add missing UI components and fix import paths

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 import { cn } from "@/lib/utils"
@@ -16,7 +18,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4 rounded-full bg-[#E5E7EB] shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
