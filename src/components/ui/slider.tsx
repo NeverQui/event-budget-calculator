8209b20cbea440a8b61b8b73f3c5ace0a97a9d25
Peter Hartman <peterhartman@mac-studio.home>
2025-04-19 23:07:51 -0400

    Update build configuration and fix dependencies

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -4,8 +4,6 @@ import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
 import { cn } from "@/lib/utils"
 
-const roundUpToNearest5 = (value: number) => Math.ceil(value / 5) * 5;
-
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
